e970219ddea97ddd9ec18795208df934a76a0570
Yurii Myronchuk <bladeroot@gmail.com>
2018-11-17 17:48:57 +0000

    csfix

--- config/params.php
+++ config/params.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * hiAPI Directi plugin
+ * hiAPI NIC.ru plugin
  *
- * @link      https://github.com/hiqdev/hiapi-directi
- * @package   hiapi-directi
+ * @link      https://github.com/hiqdev/hiapi-nicru
+ * @package   hiapi-nicru
  * @license   BSD-3-Clause
  * @copyright Copyright (c) 2017, HiQDev (http://hiqdev.com/)
  */
